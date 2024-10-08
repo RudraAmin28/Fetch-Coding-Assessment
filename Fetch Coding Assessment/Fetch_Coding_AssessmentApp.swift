b06c48f1fb34b569b25c1f1c4ecb86feb7e7a5e5
RudraAmin28 <rudrayogini@gmail.com>
2024-10-08 12:44:04 -0400

    Initial Commit

---
+++ Fetch Coding Assessment/Fetch_Coding_AssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_Coding_AssessmentApp.swift
+//  Fetch Coding Assessment
+//
+//  Created by Rudra Amin on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_Coding_AssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Fetch Coding AssessmentTests/Fetch_Coding_AssessmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_Coding_AssessmentTests.swift
+//  Fetch Coding AssessmentTests
+//
+//  Created by Rudra Amin on 10/8/24.
+//
+
+import Testing
+@testable import Fetch_Coding_Assessment
+
+struct Fetch_Coding_AssessmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
